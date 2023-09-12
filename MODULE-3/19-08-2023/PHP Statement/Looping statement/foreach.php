9f6023ba26130a38bd2933a3e4a22f84cc587404
femishadodariya <136450810+femishadodariya@users.noreply.github.com>
2023-09-12 21:00:50 +0530

    Add files via upload

---
+++ MODULE-3/19-08-2023/PHP Statement/Looping statement/foreach.php
@@ -0,0 +1,12 @@
+<?php
+// foreach () it used to convert any arry as value.
+$name=array("femish");
+$name1=array("adodariya");
+// print_r($name)
+foreach($name as $b);
+foreach($name1 as $a);
+{
+    echo "my name is $b"."<br>";
+    echo "my name is $a"."<br>";
+}
+?>
---
+++ MODULE-3/19-08-2023/PHP Statement/Looping statement/looping_statements.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+looping statements : where we print one values again and again there we apply loop
+                    or 
+                    numbers of iteration repeated again and again there we apply loop
+                                 
+types of loop 
+a) for 
+b) foreach
+c) while 
+d) do while
+
+
+*/
+?>
