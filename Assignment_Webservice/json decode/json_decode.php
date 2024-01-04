774df4097b7f880080406049eab6d60f10190158
femishadodariya <136450810+femishadodariya@users.noreply.github.com>
2024-01-04 16:48:34 +0530

    Delete Assignment_Webservice directory

--- Assignment_Webservice/json decode/json_decode.php
+++
@@ -1,5 +0,0 @@
-<?php
-// json_decode() : is used to convert any encoded data of json into string of object formate
-$country='["india","dubai","thailand"]';
-print_r(json_decode($country));
-?>
--- Assignment_Webservice/json encode/json_encode.php
+++
@@ -1,6 +0,0 @@
-<?php
-// json_encode() : encode a data of array in json formate
-$country=array("india","usa","dubai");
-echo json_encode ($country);
-
-?>
